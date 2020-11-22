c8d43a26986ccd51a452e75558f28cf1a01efc0c
lidyaallenmoses <72965147+lidyaallenmoses@users.noreply.github.com>
2020-11-22 21:36:24 +0530

    Add files via upload

---
+++ requestservice.js
@@ -0,0 +1,18 @@
+class requesterservice{
+    request = function(requestObj){
+      
+
+       /* var requesterserviceObj = new requesterservice();
+
+        value = requesterserviceObj.request(requestObj);*/
+        
+
+     /* var usersTemp = JSON.parse(localStorage.getItem("USERS"));
+     
+       var users= usersTemp ? usersTemp:[];
+       users.push(requestObj);
+       localStorage.setItem("REQUEST",JSON.stringify(users));
+       var result="success";
+       return result;*/
+        }
+}
