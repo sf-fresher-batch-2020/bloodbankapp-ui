eef4250c48d7f9c215d448569e37670e8c34f298
lidyaallenmoses <72965147+lidyaallenmoses@users.noreply.github.com>
2020-11-30 22:59:17 +0530

    Add files via upload

---
+++ registerService.js
@@ -0,0 +1,18 @@
+class registerService {
+    register = function(detail) {
+        var usersr = JSON.parse(localStorage.getItem("USERS")) || [];
+        if (!usersr.some(user => user.email === detail.email)) {
+            usersr.push(detail);
+            localStorage.setItem("USERS", JSON.stringify(usersr));
+            location.reload();
+            console.log(detail);
+            alert("continue login");
+            window.location.href = "login.html";
+            let result = "success";
+            return result;
+        } else {
+            alert("user already exists try registering with different credentials");
+        }
+    }
+}
+
